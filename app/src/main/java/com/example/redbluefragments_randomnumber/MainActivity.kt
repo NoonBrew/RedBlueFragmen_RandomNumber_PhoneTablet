da22e563c4757aacf437f0f655f00b48ad8f7cad
PearMyst <email>
2022-02-16 18:33:59 -0600

    Adding to new Rep

--- app/src/main/java/com/example/redbluefragments_randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/redbluefragments_randomnumber/MainActivity.kt
@@ -8,20 +8,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_GEN, this) {
-            requestKey, bundle -> // Swap out red fragment for blue fragment
+
+        // What size is the screen?
+        // If smaller less than 600 DP, then use listener and swap fragments.
+        // Otherwise nothing else is needed.
+
+        val deviceSmallestWidth = resources.configuration.smallestScreenWidthDp
+
+        if (deviceSmallestWidth < 600) {
+            supportFragmentManager.setFragmentResultListener(
+                RANDOM_NUMBER_GEN,
+                this
+            ) { requestKey, bundle -> // Swap out red fragment for blue fragment
                 supportFragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainerView, BlueFragment.newInstance())
+                    .add(R.id.fragment_container, BlueFragment.newInstance())
                     .addToBackStack("BLUE")
                     .commit()
-        }
+            }
 
-        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_VIEWED, this) {
-            requestKey, bundle -> // Swap out blue fragment for red fragment
+            supportFragmentManager.setFragmentResultListener(
+                RANDOM_NUMBER_VIEWED,
+                this
+            ) { requestKey, bundle -> // Swap out blue fragment for red fragment
                 supportFragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainerView, RedFragment.newInstance())
+                    .add(R.id.fragment_container, RedFragment.newInstance())
                     .addToBackStack("RED")
                     .commit()
+            }
         }
         // Options: 1. Adds, 2. Replaces.
         // 1. Pause the red fragment, add the blue fragment "on top" <- Fine for lightweight fragments
